39a568b084d5ff071d90f1ebd61c01bd905772fb
RBD Digital <hello@rbd.digital>
2026-02-09 16:00:35 +0100

    add oil

--- nvim/init.lua
+++ nvim/init.lua
@@ -577,6 +577,43 @@ require("lazy").setup({
 	},
 
 	-- ┌──────────────────────────────────────────────┐
+	-- │  Oil file browser                            │
+	-- └──────────────────────────────────────────────┘
+
+	{
+		"stevearc/oil.nvim",
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		lazy = false,
+		opts = {
+			keymaps = {
+				["g?"] = "actions.show_help",
+				["<CR>"] = "actions.select",
+				["<C-s>"] = "actions.select_vsplit",
+				["<C-h>"] = "actions.select_split",
+				["<C-t>"] = "actions.select_tab",
+				["<C-p>"] = "actions.preview",
+				["<C-c>"] = "actions.close",
+				["<C-l>"] = "actions.refresh",
+				["-"] = "actions.parent",
+				["_"] = "actions.open_cwd",
+				["`"] = "actions.cd",
+				["~"] = "actions.tcd",
+				["gs"] = "actions.change_sort",
+				["gx"] = "actions.open_external",
+				["g."] = "actions.toggle_hidden",
+				["g\\"] = "actions.toggle_trash",
+			},
+			view_options = {
+				show_hidden = true,
+			},
+		},
+		config = function(_, opts)
+			require("oil").setup(opts)
+			vim.keymap.set("n", "<leader>o", "<CMD>Oil<CR>", { desc = "Open Oil" })
+		end,
+	},
+
+	-- ┌──────────────────────────────────────────────┐
 	-- │  TailwindCSS plugins                         │
 	-- └──────────────────────────────────────────────┘
 	{
